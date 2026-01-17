5b3253c22f40b1d1639c309c173f8ad9e8061455
Victor Doucet <victor.doucet@epitech.eu>
2026-01-17 16:50:55 +0100

    [ADD] create .env file if it does not exist

--- start_server.sh
+++ start_server.sh
@@ -2,5 +2,14 @@
 
 set -e
 
+if [ ! -f ".env" ]; then
+    touch .env
+    chmod 600 .env
+
+    echo "DATABASE_DB=price" >> .env
+    echo "DATABASE_USER=price" >> .env
+    echo "DATABASE_PASSWORD=price" >> .env
+fi
+
 docker compose build
 docker compose up $@
